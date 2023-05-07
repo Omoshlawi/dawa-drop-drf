10f614936b7b9450b7412be02f7eeda00e3b43b4
Laurent Ouma <lawiomosh3@gmail.com>
2023-05-07 21:47:43 +0300

    aDDED SOME DOC string for reminder

--- core/consumers.py
+++ core/consumers.py
@@ -83,11 +83,22 @@ class TripConsumer(WebsocketConsumer):
 class AsyncTripConsumer(AsyncWebsocketConsumer):
     """
     Very similar to above except for it is asynchronous
+     if AsyncTripConsumer did access Django models or other synchronous code it
+     would still be possible to rewrite it as asynchronous.
+     Utilities like asgiref.sync.sync_to_async and channels.db.database_sync_to_async
+     can be used to call synchronous code from an asynchronous consumer.
+     The performance gains however would be less than if it only used async-native libraries.
+
+     https://channels.readthedocs.io/en/stable/topics/databases.html
+     NB: ADD IT AS A METHOD DECORATED IN THIS CLASS OR JUST PASS ITS REFERENCE TO DB ASYNC,
+     IMPORTANT THING IS TO INCLUDE IT AS A METHOD T
     """
 
     async def connect(self):
         self.id = self.scope['url_route']['kwargs']['trip_id']
         self.trip_group_name = 'Trip_%s' % self.id
+        self.user = self.scope['user']
+        # print(self.user)
 
         # Join room group
         await self.channel_layer.group_add(self.trip_group_name, self.channel_name)
@@ -103,12 +114,30 @@ class AsyncTripConsumer(AsyncWebsocketConsumer):
         # text_data_json = json.loads(text_data)
         # message = text_data_json["message"]
 
+        """
+        NB: The dictionary can contain any keys and values that the developer wants to
+        pass to the group.
+        However, the type key is commonly used in Channels to specify the type of
+        message being sent. This key is not predefined, but it is a common convention
+        to use it to indicate the type of the message being sent, so that the
+        appropriate handler function can be called on the receiving end. In this
+        case, the type key is set to 'trip_tracking_info' to indicate that the message
+        is related to trip tracking information.
+
+        eg:
+            {
+                'my_type': 'trip_tracking_info',
+                'message': text_data,
+             }
+             Then, you would need to use the same key name 'my_type' when you
+             handle the message in the consumers or in the routing code.
+        """
         # Send message to room group
         await self.channel_layer.group_send(
             self.trip_group_name,
             {
                 'type': 'trip_tracking_info',
-                'message': text_data,
+                'info': text_data,
             }
         )
 
